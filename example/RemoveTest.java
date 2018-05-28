c04c9910256658031673614cf1d99931a25c1e44
Michael Andonie <michael@andonie.net>
2018-05-28 20:54:35 +0200

    Big Ass Commit: - Removed non-used Demos - Translated all Demos to the new (final) 4.0 interfaces - Implemented a Central Scene from which all Demos can be requested - Minor conveniences added (Scroll Features etc.)

--- example/RemoveTest.java
+++ example/RemoveTest.java
@@ -1,3 +1,5 @@
+package ea.showcase;
+
 import ea.*;
 
 /**
--- src/ea/showcase/BallThrow.java
+++ src/ea/showcase/BallThrow.java
@@ -1,4 +1,13 @@
+package ea.showcase;
+
 import ea.*;
+import ea.actor.Rectangle;
+import ea.collision.CollisionListener;
+import ea.actor.*;
+import ea.handle.Physics;
+import ea.keyboard.*;
+
+import java.awt.*;
 
 /**
  * Eine einfache Demonstration der Engine-Physik durch eine Ball-Wurf-Simulation. Es wird ein Ball (durch Wirkung
@@ -8,10 +17,10 @@ import ea.*;
  *
  * <p>Die Simulation kann gesteuert werden durch:</p>
  * <ul>
- *     <li>S-Taste: Startet Simulation</li>
- *     <li>R-Taste: Setzt Simulation zurück</li>
- *     <li>Die Tasten Z und U ändern den Zoom auf die Umgebung (rudimentär implementiert)</li>
- *     <li>D-Taste: Toggelt den Debug-Modus (zeigt das Pixel-Raster)</li>
+ *     <li>S-Key: Startet Simulation</li>
+ *     <li>R-Key: Setzt Simulation zurück</li>
+ *     <li>Die Keyn Z und U ändern den Zoom auf die Umgebung (rudimentär implementiert)</li>
+ *     <li>D-Key: Toggelt den Debug-Modus (zeigt das Pixel-Raster)</li>
  * </ul>
  *
  * <h3>Anpassung der Parameter</h3>
@@ -19,7 +28,7 @@ import ea.*;
  * <p>Die Simulation arbeitet mit einigen physikalischen Parametern, die sich ändern lassen. Folgende Parameter sind
  * als Konstanten im Code definiert und können im angepasst werden:</p>
  * <ul>
- *     <li><code>DURCHMESSER</code>: Der Durchmesser des Kreises (hat keinen Einfluss auf die Masse.</li>
+ *     <li><code>DURCHMESSER</code>: Der Durchmesser des Circlees (hat keinen Einfluss auf die Masse.</li>
  *     <li><code>HOEHE_UEBER_BODEN</code>: Abstand zwischen dem untersten Punkt des Balls und dem Boden</li>
  *     <li><code>MASSE</code>: Masse des Balls</li>
  *     <li><code>IMPULS: Impuls, der auf den Ball angewandt wird.</code></li>
@@ -29,22 +38,18 @@ import ea.*;
  * Created by Michael on 11.04.2017.
  */
 public class BallThrow
-        extends Game
-implements KollisionsReagierbar {
-
-    public static void main(String[] args) {
-        new BallThrow();
-    }
+        extends ShowcaseDemo
+implements CollisionListener, KeyListener {
 
     /**
-     * Der Kreis. Auf ihn wird ein Impuls gewirkt.
+     * Der Circle. Auf ihn wird ein Impuls gewirkt.
      */
-    private Kreis ball;
+    private Circle ball;
 
     /**
      * Der Boden.
      */
-    private Rechteck boden;
+    private Rectangle boden;
 
     /**
      * Der Startzeitpunkt der Simulation. Für Zeitmessung
@@ -68,59 +73,57 @@ implements KollisionsReagierbar {
 
     private static final float BODEN_TIEFE = 700, ABSTAND_LINKS=50;
 
-    public BallThrow() {
-        super(1200, 720, "EA Ballwurf-Simulation");
+    public BallThrow(Scene parent, int width, int height) {
+        super(parent);
+        initialisieren();
+        addKeyListener(this);
     }
 
-    @Override
     public void initialisieren() {
         //Zuallererst die Größenordnung klar machen (muss vor Erstellung der Objekte geschehen)
-        this.ppmSetzen(PIXELPROMETER);
+        getWorldHandler().setPixelProMeter(PIXELPROMETER);
 
         //Den Ball erstellen
-        ball = new Kreis(0,0, DURCHMESSER*PIXELPROMETER);
-        wurzel.add(ball);
-        ball.setColor("Rot");
-        ball.physik.typ(Physik.Typ.DYNAMISCH);
-        ball.physik.masse(MASSE);
-        ball.position.mittelpunktSetzen(ABSTAND_LINKS,
+        ball = new Circle( DURCHMESSER*PIXELPROMETER);
+        add(ball);
+        ball.setColor(Color.RED);
+        ball.physics.setType(Physics.Type.DYNAMIC);
+        ball.physics.setMass(MASSE);
+        ball.position.setCenter(ABSTAND_LINKS,
                 BODEN_TIEFE-(HOEHE_UEBER_BODEN*PIXELPROMETER + 0.5f*DURCHMESSER*PIXELPROMETER));
 
         //kamera.fokusSetzen(ball);
 
         //Den Boden erstellen
-        boden=new Rechteck(0,BODEN_TIEFE, 20000,20);
-        wurzel.add(boden);
-        boden.setColor("Weiss");
-        boden.physik.typ(Physik.Typ.STATISCH);
+        boden=new Rectangle(20000,20);
+        boden.position.set(0,BODEN_TIEFE);
+        add(boden);
+        boden.setColor(Color.WHITE);
+        boden.physics.setType(Physics.Type.STATIC);
 
         //Kollision zwischen Ball und Boden beobachten (Code ist uns egal, wir kennen nur einen Kollisionsfall)
-        anmelden.kollisionsReagierbar(this, ball, boden);
+        addCollisionListener(this, ball, boden);
     }
 
     /**
-     * Wird bei jedem Tastendruck ausgeführt.
-     * @param code Der eindeutige Code der gedrückten Taste.
+     * Wird bei jedem Keyndruck ausgeführt.
+     * @param code Der eindeutige Code der gedrückten Key.
      */
     @Override
-    public void tasteReagieren(int code) {
+    public void onKeyDown(int code) {
         switch(code) {
-            case Taste.S: //Starte die Simulation
+            case Key.S: //Starte die Simulation
                 simulationStarten();
                 break;
-            case Taste.D: //Toggle den Debug Modus
-                EngineAlpha.setDebug(!EngineAlpha.isDebug());
-                break;
-            case Taste.Z: //Zoom Out
-                kamera.zoomSetzen(kamera.getZoom()-0.1f);
-                break;
-            case Taste.U: //Zoom In
-                kamera.zoomSetzen(kamera.getZoom()+0.1f);
-                break;
-            case Taste.R: //Reset
+            case Key.R: //Reset
                 simulationZuruecksetzen();
                 break;
         }
+    }
+
+    @Override
+    public void onKeyUp(int i) {
+        //Ignore
     }
 
     /**
@@ -131,12 +134,12 @@ implements KollisionsReagierbar {
         startzeit=System.currentTimeMillis();
 
         //Schwerkraft auf den Ball wirken lassen
-        ball.physik.schwerkraft(new Vektor(0, 9.81f));
+        ball.physics.setGravity(new Vector(0, 9.81f));
 
         //Impuls berechnen und auf den Ball wirken lassen
-        Vektor impuls = new Vektor((float)Math.cos(Math.toRadians(WINKEL))*IMPULS,
+        Vector impuls = new Vector((float)Math.cos(Math.toRadians(WINKEL))*IMPULS,
                 (float)-Math.sin(Math.toRadians(WINKEL))*IMPULS);
-        ball.physik.impulsWirken(impuls);
+        ball.physics.applyImpulse(impuls);
     }
 
     /**
@@ -144,29 +147,34 @@ implements KollisionsReagierbar {
      * zurückgesetzt und der Ball wird in Ruhe versetzt.
      */
     private void simulationZuruecksetzen() {
-        ball.physik.schwerkraft(new Vektor(0,0)); //Schwerkraft deaktivieren
-        ball.position.mittelpunktSetzen(ABSTAND_LINKS, //Ballposition zurücksetzen
+        ball.physics.setGravity(new Vector(0,0)); //Schwerkraft deaktivieren
+        ball.position.setCenter(ABSTAND_LINKS, //Ballposition zurücksetzen
                 BODEN_TIEFE-(HOEHE_UEBER_BODEN*PIXELPROMETER + 0.5f*DURCHMESSER*PIXELPROMETER));
-        ball.physik.inRuheVersetzen(); //Ball in Ruhe versetzen
+        ball.physics.cancelAll(); //Ball in Ruhe versetzen
     }
 
     /**
-     * Wird bei jeder Kollision zwischen <b>mit diesem Interface angemeldeten</b> <code>Raum</code>-Objekten
+     * Wird bei jeder Kollision zwischen <b>mit diesem Interface angemeldeten</b> <code>Actor</code>-Objekten
      * aufgerufen.
      * @param colliding  Das Kollidierende Objekt
      */
     @Override
-    public void kollision(Raum colliding) {
+    public void onCollision(Actor colliding) {
         //Kollision bedeutet, dass der Ball auf den Boden gefallen ist => Zeitmessung abschließen
         long endzeit = System.currentTimeMillis();
         long zeitdifferenz = endzeit-startzeit;
 
         //Zurückgelegte Distanz seit Simulationsstart ausmessen (Pixel-Differenz ausrechnen und auf Meter umrechnen)
-        float distanz = (ball.position.mittelPunkt().realX()-ABSTAND_LINKS) / PIXELPROMETER;
+        float distanz = (ball.position.getCenter().getRealX()-ABSTAND_LINKS) / PIXELPROMETER;
 
         //Messungen angeben
         System.out.println("Der Ball ist auf dem Boden aufgeschlagen. Seit Simulationsstart sind " +
          + (zeitdifferenz/1000) + " Sekunden und " + (zeitdifferenz%1000) + " Millisekunden vergangen.\n" +
                 "Der Ball diese Distanz zurückgelegt: " + distanz + " m");
+    }
+
+    @Override
+    public void onCollisionEnd(Actor colliding) {
+        //Ignore
     }
 }
